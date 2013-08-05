faaf08db19f55a4e17ff029aec92f0361999fdda
Eric Heitmuller <eheitmuller@gmail.com>
2013-08-05 00:27:37 -0700

    initial commit

---
+++ SpecialCore/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialCore
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Level { get; set; }
+
+        //attributes
+        public int Strength { get; set; }
+        public int Perception { get; set; }
+        public int Endurance { get; set; }
+        public int Charisma { get; set; }
+        public int Intelligence { get; set; }
+        public int Agility { get; set; }
+        public int Luck { get; set; }
+
+        //skills
+        public int Lockpick { get; set; }
+
+    }
+}
---
+++ SpecialTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpecialCore;
+
+namespace SpecialTest
+{
+    internal class Program
+    {
+        private static Character Ted;
+        private static Character Bill;
+        private static Character You;
+
+        private enum GameState
+        {
+            Start,
+            Running,
+            End
+        }
+
+        private static GameState _currentState;
+
+        static void Main(string[] args)
+        {
+            _currentState = GameState.Start;
+            Initialize();
+
+            while (_currentState == GameState.Running)
+            {
+
+            }
+
+            Cleanup();
+        }
+
+        private static void Cleanup()
+        {
+            Console.WriteLine("Cleanup");
+        }
+
+        private static void Initialize()
+        {
+            Console.WriteLine("Init");
+
+            Ted = new Character
+                {
+                    Name = "Ted",
+                    Hp = 10,
+
+                    Strength = 
+                }
+
+
+            _currentState = GameState.Running;
+        }
+    }
+}
